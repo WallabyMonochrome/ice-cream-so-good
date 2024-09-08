d64ec24c50b9ec6883aa4047585d16c7d994bd38
Thib <introvigne.t@gmail.com>
2024-09-08 19:51:07 -0400

    Update

--- src/components/3D/BigIceCream.tsx
+++ src/components/3D/BigIceCream.tsx
@@ -1,20 +1,31 @@
-import { Box, Center, Environment, OrbitControls, PerspectiveCamera, View, useGLTF } from "@react-three/drei"
+import {Box, Center, Environment, OrbitControls, PerspectiveCamera, View, useGLTF} from "@react-three/drei"
+
+
+const IceBalls = () => {
+  const {scene} = useGLTF("/models/icecream.glb");
+  return (
+    <>
+      <primitive position={[0, 1, 0]} object={scene} scale={0.7} onClick={() => console.log("Model Clicked")}/>
+
+    </>
+  )
+}
 
 export const BigIceCream = () => {
   // Load the GLTF model (adjust the path as per your project structure)
-  const { scene } = useGLTF("/models/icecream.glb");
-  const sceneCup= useGLTF("/models/cup.glb");
+  const sceneCup = useGLTF("/models/cup.glb");
 
   return (
     <>
       {/*// @ts-ignore */}
-      <View style={{ width: "inherit", height: "inherit" }}>
+      <View style={{width: "inherit", height: "inherit"}}>
         <Center>
-          <OrbitControls makeDefault />
+          <OrbitControls makeDefault/>
           {/* Display the GLTF model */}
-          <primitive  position={[0,1, 0]} object={scene} scale={0.7} onClick={() => console.log("Model Clicked")} />
-          <primitive position={[0,-1, 0]} object={sceneCup.scene} scale={1} onClick={() => console.log("Model Clicked")} />
-          <Environment  preset="sunset" />
+          <IceBalls />
+          <primitive position={[0, -1, 0]} object={sceneCup.scene} scale={1}
+                     onClick={() => console.log("Model Clicked")}/>
+          <Environment preset="sunset"/>
         </Center>s
       </View>
     </>
@@ -22,4 +33,5 @@ export const BigIceCream = () => {
 }
 
 // Ensure GLTFLoader is cached and available
-useGLTF.preload("/models/iceCream.glb");
+useGLTF.preload("/models/icecream.glb");
+useGLTF.preload("/models/cup.glb");
